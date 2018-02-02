c14075094b2769909ba9749695c6faa5235b6677
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2018-02-02 10:49:26 +0700

    Bai05

---
+++ Bai05/code.py
@@ -0,0 +1,14 @@
+class InOutString(object):
+    def __init__(seft):
+        seft.s = ""
+
+    def getString(seft):
+        seft.s = input("Nhap chuoi:")
+
+    def printString(seft):
+        print(seft.s.upper())
+
+
+value = InOutString()
+value.getString()
+value.printString()
